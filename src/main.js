45e9ff8dd0511449cf5f0ce5956ab59391c6a4cb
Devon <devon.stanton@gmail.com>
2022-06-09 19:44:06 +0200

    successfully connected main to steam and airtable updates

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { findRecord, updateRecord } from './airtableRequest.js'
+import { findGame } from './steamRequest.js'
 
 const data = {} // data record to input necessary fields for updates
 
@@ -7,14 +8,29 @@ const recordID = 'recVGaP4TAlHh7W6y' // recieve record ID from webhook
 // get record
 let game = await findRecord(recordID)
 
+// collect information on record ahead of steam request
 if(game) {
     
     data.Platform = game.get('Platform') // adding in existing Genre's (avoids deletion)
     !data.Platform.includes('Windows PC') ? data.Platform.push('Windows PC') : console.log('Windows PC already exists in Platform')  // adding 'Windows PC' since content is coming from Steam
-    
+    data['Store page'] = game.get('Store page')
     
 } else {
     console.log(`Did not find the record with ID: ${recordID}`)
 }
+
+// find steam data
+const gameData = await findGame(data['Store page'])
+// load data
+let genres = []
+data['Project Name'] = gameData.name
+data['Expected Release'] = gameData.release_date.date
+data['Website'] = gameData.website
+data['Studio Name'] = gameData.developers.join(',')
+data['About'] = gameData.detailed_description
+// for(let genre in gameData.genres){
+//     genres.push(genre.description)
+// }
+// data['Genre'] = genres
 
 updateRecord(recordID, data) // update base with new data
--- src/steamRequest.js
+++ src/steamRequest.js
@@ -2,11 +2,25 @@ import SteamAPI from 'steamapi'
 import 'dotenv/config' 
 const steam = new SteamAPI(process.env.STEAM_API_KEY);
 
-const gameID = '1335830'
+// const gameID = '1335830' // test game
 
-export const findGame = async (gameID) => {
-    const game = await steam.getGameDetails(gameID)
-    console.log(`Game Name: ${game.name} \nWebsite: ${game.website} \nStudio(s): ${game.developers[0]}`); 
+
+// extract game id from url
+const regex = /([0-9]{7})/;
+
+export const findGame = async (gameURL) => {
+    const gameID = regex.exec(gameURL)
+    console.log(`From ${gameURL} \nGame ID: ${gameID}`)
+
+
+    try {
+        const game = await steam.getGameDetails(gameID[0])
+        console.log(`Game Name: ${game.name} \nWebsite: ${game.website} \nStudio(s): ${game.developers[0]}`); 
+        return game;
+    } catch(e) {
+        console.log(`${e.message}`)
+    }
+   
 };
 
-findGame(gameID) // test function
+// findGame(gameID) // test function
