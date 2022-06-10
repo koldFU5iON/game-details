75166fa2df3e339b4476065445a9235b2acf3c0f
Devon <devon.stanton@gmail.com>
2022-06-10 17:01:02 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.listen(
 app.get('/steam-game/:id',(req,res) => {
     const { id } = req.params
     if(!id) {
-        res.status(200).send(`couldn't not read ID : ${id}`)
+        res.status(200).send(`could not read ID : ${id}`)
     } else{
         res.status(200).send(`successfully recieved ${id}`)
         steamGame(id)
