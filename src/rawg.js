b33590f5725e6819b3502cd49ba323c127fb4898
Devon Stanton <devon.stanton@gmail.com>
2022-07-25 11:11:10 +0200

    successfully called on api to rawg;

--- src/rawg.js
+++ src/rawg.js
@@ -1,13 +1,35 @@
 import 'dotenv/config'
+import fetch from 'node-fetch'
 
 const rawgURL = "https://api.rawg.io/api/"
 const api = `?key=${process.env.RAWG_API_KEY}`
 
-
-
 // GET https://api.rawg.io/api/platforms?key=YOUR_API_KEY
 // GET https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
 
-const game = async () => {
+const getGame = async (name, type="games") => {
+// let xmlHttp = new XMLHttpRequest();
+
+const url = rawgURL + type + api;
+
+const response = await fetch(url);
+const data = await response.json();
+
+console.log(data)
+    // get request
+//     function httpGetAsync(theUrl, callback)
+// {
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.onreadystatechange = function() { 
+//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+//             callback(xmlHttp.responseText);
+//     }
+//     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+//     xmlHttp.send(null);
+// }
 
 }
+
+// test call
+
+getGame("ori")
