dcf2af75b16c45e897f4baeecccfa15c273f0bed
Devon Stanton <devon.stanton@gmail.com>
2022-08-01 11:36:09 +0200

    cleaned up formatting

--- src/airtable_api.js
+++ src/airtable_api.js
@@ -1,33 +1,35 @@
-import Airtable from 'airtable';
-import 'dotenv/config';
+import Airtable from "airtable";
+import "dotenv/config";
 const env = process.env;
 
-const base = new Airtable({apiKey:env.AIRTABLE_API_KEY}).base(env.AIRTABLE_BASE_ID);
+const base = new Airtable({ apiKey: env.AIRTABLE_API_KEY }).base(
+  env.AIRTABLE_BASE_ID
+);
 
-const table = base('tblMXiuJrT9lvXo6U');
+const table = base("tblMXiuJrT9lvXo6U");
 
 export const findRecord = async (recordID) => {
-    try {
-        return await table.find(recordID)
-    } catch(err) {
-        console.log(`${err.message} : ${err.error}`)
-    }
-} 
+  try {
+    return await table.find(recordID);
+  } catch (err) {
+    console.log(`${err.message} : ${err.error}`);
+  }
+};
 
 // update record with ID
 export const updateRecord = async (recordID, data) => {
-            table.update(recordID, data, {typecast: true}, (err, record) => {
-                if(!err) {
-                    console.log(`Record was updated successfully : ${record.getId()}`) //.get('Project Name')
-                } else {
-                    console.error(`${err.message} : ${err.error}`);
-                }
-            })
-        }
+  table.update(recordID, data, { typecast: true }, (err, record) => {
+    if (!err) {
+      console.log(`Record was updated successfully : ${record.getId()}`); //.get('Project Name')
+    } else {
+      console.error(`${err.message} : ${err.error}`);
+    }
+  });
+};
 
-export const createField = async(recordID, data, field) => {
-    table.update(recordID, 'Genre',  )
-}
+export const createField = async (recordID, data, field) => {
+  table.update(recordID, "Genre");
+};
 
 // {
 //     "id": "rectL3Mp4Dg6MX2y8",
@@ -38,5 +40,5 @@ export const createField = async(recordID, data, field) => {
 // }
 
 export const updateRecords = async (records) => {
-    table.update(records)
-}
+  table.update(records);
+};
